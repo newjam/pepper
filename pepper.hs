18b6aac214c3e7322b3fac11155963ee62f5d022
Ubuntu <ubuntu@ip-10-179-42-109.ec2.internal>
2013-08-14 04:33:48 +0000

    added nav bar

--- pepper.hs
+++ pepper.hs
@@ -18,10 +18,8 @@ import Text.Blaze.Html.Renderer.Text
 import qualified Data.ByteString.Char8 as C
 
 import qualified Data.Text as T
-import qualified Data.Text.IO as T
-import Data.Text.Encoding as TE
+import Data.Text.Encoding as T
 
-import qualified Data.Text.Lazy as LT
 import qualified Data.Text.Lazy.IO as LT
 
 import Data.Attoparsec.ByteString.Char8
@@ -35,9 +33,9 @@ import Data.Maybe (mapMaybe)
 
 import qualified Data.ByteString.Lazy as BL
 
-import Data.String
-
 import Data.Semigroup
+
+import Web.Encodings (encodeUrl, decodeUrl)
 
 instance Semigroup C.ByteString where
   (<>) = mappend
@@ -98,7 +96,7 @@ sigfig x = fromIntegral (round (x * 100.0)) / 100.0
 instance ToMarkup Player where
   toMarkup (Player name) = H.a
     ! A.class_ "player"
-    ! A.href (textValue $ "/player/"<>name)
+    ! A.href (textValue $ "/player/"<>encodeUrl name)
     $ H.toHtml name
 
 instance ToMarkup PlayerRank where
@@ -143,9 +141,11 @@ instance ToMarkup MatchOutcome where
           else ("loser", "winner")
       H.td ! A.class_ p1Class $ H.toHtml p1
       H.td ! A.class_ p2Class $ H.toHtml p2
-      let oddsClass = if odds >= 0.5
+      let oddsClass = if odds >= 0.6
           then "correct"
-          else "incorrect"
+          else if odds <= 0.4
+            then "incorrect"
+            else "ok"
       H.td ! A.class_ oddsClass $ H.toHtml odds
 
 instance ToMarkup CurrentMatch where
@@ -173,14 +173,14 @@ fucking r = r >>= maybe
 getRanks index count = do
   R.select 1
   r <- fuck $ R.zrevrangeWithscores "players" index (index + count - 1)
-  let f rank (name, score) = PlayerRank (Player . TE.decodeUtf8 $ name) rank (Rating score)
+  let f rank (name, score) = PlayerRank (Player . T.decodeUtf8 $ name) rank (Rating score)
   return . RankBoard index .  zipWith f [index + 1 ..] $ r   
 
 getRecent :: R.Redis MatchList -- [MatchOutcome]
 getRecent = getMatches "history" 0 20
 
 getPlayerMatches :: C.ByteString -> R.Redis MatchList
-getPlayerMatches player = getMatches key 0 10 where
+getPlayerMatches player = getMatches key 0 20 where
   key = "games["<>player<>"]"
 
 getMatches :: C.ByteString -> Integer -> Integer -> R.Redis MatchList
@@ -196,12 +196,12 @@ getCurrent = CurrentMatch <$> p1 <*> p2 <*> odds where
   odds = Odds . parseDouble <$> get "odds"
   parseDouble = either (error) id . parseOnly double
   get key = fucking . fuck $ R.get key
-  mkPlayer = Player . TE.decodeUtf8
+  mkPlayer = Player . T.decodeUtf8
 
 
 getRank :: Player -> R.Redis PlayerRank
 getRank (Player name) = PlayerRank (Player name) <$> rank <*> rating where
-  key = TE.encodeUtf8 name
+  key = T.encodeUtf8 name
   rank = fucking . fuck $ R.zrank "players" key
   rating = Rating <$> (fucking . fuck $ R.zscore "players" key)
 
@@ -231,14 +231,15 @@ routes conn = do
       H.toHtml rankings
   get "/ranks" $ redirect "/history" 
   get "/player/:player" $ \playerNameBS -> do
-    let player = Player . TE.decodeUtf8 $ playerNameBS
-    matches <- liftIO . R.runRedis conn $ getPlayerMatches playerNameBS
+    let decoded = decodeUrl playerNameBS
+    let player = Player . T.decodeUtf8 $ decoded
+    matches <- liftIO . R.runRedis conn $ getPlayerMatches decoded
     PlayerRank _ _ rating <- liftIO . R.runRedis conn $ getRank player
     
     html . renderHtml . page $ do
-      H.h2 $ H.toHtml player `mappend` "'s Matches"
-      --"Rank "
-      --H.toHtml i
+      H.h2 $ do
+        H.toHtml player
+        "'s Matches"
       H.h4 $ do
         "Elo rating of "
         H.toHtml rating
@@ -252,7 +253,7 @@ routes conn = do
     html . renderHtml $ page $ do
       H.h2 $ do "Current Match"
       H.toHtml current
-      H.h2 $ do "History"
+      H.h2 $ do "Recent Matches"
       H.div $ H.toHtml recent
 
 page content = do
@@ -261,11 +262,12 @@ page content = do
       H.title "Pepper"
       H.link ! A.rel "stylesheet" ! A.type_ "text/css" ! A.href "/style.css"
     H.body $ do
-      H.div $ do
-        H.a ! A.href "/scoreboard" $ "scoreboard"
-        " | "
-        H.a ! A.href "/history" $ "history"
-        " | "
-        H.a ! A.href "/about" $ "about"
-      content
+      H.header $ do
+        H.span ! A.class_ "title" $ do "Pepper"
+        H.nav $ do
+          H.ul $ do
+            H.li $ H.a ! A.href "/scoreboard" $ "scoreboard"
+            H.li $ H.a ! A.href "/history" $ "history"
+            H.li $ H.a ! A.href "/about" $ "about"
+      H.section $ content
 
